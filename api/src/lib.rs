6f560f6aec4880734159816d689ca442b27b1767
maxlestage <maxlestage@icloud.com>
2023-03-12 01:26:53 +0100

    first commit for salvo back-api-codomaton

---
+++ api/src/lib.rs
@@ -0,0 +1,38 @@
+// use database_connection::db_connection;
+use db::db_connection::db_connection;
+use jsonwebtoken::{self, EncodingKey};
+use salvo::{__private::tracing, jwt_auth::QueryFinder, prelude::*};
+
+#[handler]
+async fn hello_world() -> &'static str {
+    "Hello there!"
+}
+
+#[handler]
+
+async fn hello_by_id(req: &mut Request) -> String {
+    req.params().get("id").cloned().unwrap_or_default()
+}
+
+#[tokio::main]
+pub async fn main() {
+    tracing_subscriber::fmt().init();
+    tracing::info!("Listening on http://0.0.0.0:7878");
+
+    db_connection().await.expect("Error");
+
+    let auth_handler: JwtAuth<JwtClaims> = JwtAuth::new(SECRET_KEY.to_owned())
+        .with_finders(vec![Box::new(QueryFinder::new("jwt_token"))])
+        .with_response_error(false);
+
+    // Define Routing tree
+    let routing = Router::with_path("")
+        .get(hello_world)
+        .push(Router::with_path("<id>").get(hello_by_id));
+
+    // Server Ready
+    Server::new(TcpListener::bind("0.0.0.0:7878"))
+        .serve(Router::with_hoop(auth_handler).handle(index))
+        // .serve(routing)
+        .await;
+}
---
+++ db/src/db_connection.rs
@@ -0,0 +1,16 @@
+use std::time::Duration;
+
+use sea_orm::{ConnectOptions, Database, DatabaseConnection, DbErr};
+
+pub async fn db_connection() -> Result<DatabaseConnection, DbErr> {
+    let mut opt = ConnectOptions::new("postgres://postgres:codo_maton@localhost:5432".to_owned());
+    opt.acquire_timeout(Duration::from_secs(10))
+        .sqlx_logging(true);
+
+    let db: DatabaseConnection = Database::connect(opt)
+        .await
+        .expect("Not connected to the database.");
+
+    dbg!(Ok(db))
+    // Ok(db)
+}
