efc3008ff399e5a2919057a90dd93d2ff53db7b8
maxlestage <maxlestage@icloud.com>
2023-03-13 12:10:29 +0100

    Create user work!

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -54,23 +54,23 @@ pub async fn main() {
     tracing_subscriber::fmt().init();
     tracing::info!("Listening on http://0.0.0.0:7878");
 
-    db_connection().await.expect("Error");
+    // db_connection().await.expect("Error");
 
-    // let auth_handler: JwtAuth<JwtClaims> = JwtAuth::new(SECRET_KEY.to_owned())
-    //     .with_finders(vec![Box::new(QueryFinder::new("jwt_token"))])
-    //     .with_response_error(false);
+    let auth_handler: JwtAuth<JwtClaims> = JwtAuth::new(SECRET_KEY.to_owned())
+        .with_finders(vec![Box::new(QueryFinder::new("jwt_token"))])
+        .with_response_error(false);
 
     // // Define Routing tree
     // let routing = Router::with_path("")
     //     .get(hello_world)
     //     .push(Router::with_path("<id>").get(hello_by_id));
 
-    let routing = Router::with_path("/signup").post(sign_up);
+    let routing = Router::with_path("signup").post(sign_up);
     // .push(Router::with_path("<id>").get(hello_by_id));
 
     // Server Ready
     Server::new(TcpListener::bind("0.0.0.0:7878"))
-        // .serve(Router::with_hoop(auth_handler).handle(index))
+        .serve(Router::with_hoop(auth_handler).handle(index))
         .serve(routing)
         .await;
 }
